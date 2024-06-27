959d01adfbd793963e5435412cda9d4dca9e2d1d
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-27 08:59:17 +0300

    Add project files.

---
+++ Leaderboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leaderboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

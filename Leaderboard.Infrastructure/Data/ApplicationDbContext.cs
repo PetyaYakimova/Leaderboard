98fc422a00f2770d11650be011bc91a8625f5f30
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-28 13:30:12 +0300

    Add project references, move db context and add iitial migration

--- Leaderboard.Infrastructure/Data/ApplicationDbContext.cs
+++ Leaderboard.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Leaderboard.Data
+namespace Leaderboard.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Leaderboard/Extensions/ServiceCollectionExtension.cs
+++ Leaderboard/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,4 @@
-﻿using Leaderboard.Data;
+﻿using Leaderboard.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 

adf36c2955b235e8a7cd67a274a0bc70ffa56e73
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-30 08:25:11 +0300

    Add organization and contest controller

---
+++ Leaderboard/Controllers/ContestController.cs
@@ -0,0 +1,22 @@
+﻿using Leaderboard.Core.Models.Contest;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaderboard.Controllers
+{
+    [Authorize]
+    public class ContestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Results(Guid contestId)
+        {
+            return View(new ContestResultsViewModel());
+        }
+    }
+}
---
+++ Leaderboard/Controllers/OrganizationContrroller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaderboard.Controllers
+{
+    [Authorize]
+    public class OrganizationContrroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

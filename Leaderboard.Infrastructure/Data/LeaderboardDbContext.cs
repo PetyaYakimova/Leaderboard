e1f30c97d313c9cd017b4556750a24c0ae8692ff
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-30 08:25:33 +0300

    Rename Db context

---
+++ Leaderboard.Infrastructure/Data/LeaderboardDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Leaderboard.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leaderboard.Infrastructure.Data
+{
+    public class LeaderboardDbContext : IdentityDbContext
+    {
+        public LeaderboardDbContext(DbContextOptions<LeaderboardDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Organization> Organizations { get; set; } = null!;
+
+		public DbSet<Contest> Contests { get; set; } = null!;
+
+		public DbSet<Team> Teams { get; set; } = null!;
+
+		public DbSet<Point> Points { get; set; } = null!;
+    }
+}

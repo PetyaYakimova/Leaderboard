6065f2bcb8c5e331eb2b0c8d06365e9983481f5a
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-03 09:23:24 +0300

    Update models and add messages constants

--- Leaderboard.Core/Models/Contest/ContestFormViewModel.cs
+++ Leaderboard.Core/Models/Contest/ContestFormViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace Leaderboard.Core.Models.Contest
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static Leaderboard.Infrastructure.Constants.DataConstants;
+using static Leaderboard.Core.Contants.MessagesConstants;
+
+namespace Leaderboard.Core.Models.Contest
 {
+	/// <summary>
+	/// A view model for the form when editing and creating a contest. Added validation attributes.
+	/// </summary>
 	public class ContestFormViewModel
 	{
+		[Required(ErrorMessage = RequieredMessage)]
+		[StringLength(ContestNameMaxLength,
+			MinimumLength = ContestNameMinLength,
+			ErrorMessage = StringLengthBetweenValuesMessage)]
+		public string Name { get; set; } = string.Empty;
+
+		[StringLength(ContestDescriptionMaxLength,
+			ErrorMessage = StringLengthNoMoreThanValueMessage)]
+		public string? Description { get; set; }
+
+		[Required(ErrorMessage = RequieredMessage)]
+		[Display(Name = "Is Active")]
+		public bool IsActive { get; set; }
 	}
 }
--- Leaderboard.Core/Models/Organization/OrganizationPreviewModel.cs
+++ Leaderboard.Core/Models/Organization/OrganizationPreviewModel.cs
@@ -1,7 +1,9 @@
-﻿namespace Leaderboard.Core.Models.Organization
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leaderboard.Core.Models.Organization
 {
     /// <summary>
-    /// View model only for displaying the information for the organization. No added validations.
+    /// View model only for displaying the information for the organization. No added validation attributes.
     /// </summary>
     public class OrganizationPreviewModel
     {
@@ -9,8 +11,10 @@
 
         public string Name { get; set; } = string.Empty;
 
+        [Display(Name="Number of contests:")]
         public int NumberOfContests { get; set; }
-        
+
+        [Display(Name="Number of administrators:")]
         public int NumberOfAdministrators { get; set; }
     }
 }

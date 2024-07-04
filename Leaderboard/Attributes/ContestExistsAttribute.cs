f5e4db891e3401e9d7ff7f356a33d21f1d2ab014
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-04 10:02:06 +0300

    Add empty custom attributes

---
+++ Leaderboard/Attributes/ContestExistsAttribute.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Leaderboard.Attributes
+{
+	public class ContestExistsAttribute : ActionFilterAttribute
+	{
+		//TODO: Check how it is done in the ChoreographyBuilder
+	}
+}

6664b7912f32da863e938992f20f3fc8ae22cdf6
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-06 08:39:47 +0300

    Add limit constants and fix the folder name for constants

---
+++ Leaderboard.Core/Constants/LimitConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Leaderboard.Core.Constants
+{
+	public static class LimitConstants
+	{
+		public const int DefaultNumberOfItemsPerPage = 10;
+	}
+}
---
+++ Leaderboard.Core/Constants/MessagesConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Leaderboard.Core.Constants
+{
+	public static class MessagesConstants
+	{
+		public const string RequieredMessage = "The {0} field is required.";
+
+		public const string StringLengthBetweenValuesMessage = "The {0} field must be between {2} and {1} characters long.";
+
+		public const string StringLengthNoMoreThanValueMessage = "The {0} field must be no more than {1} characters long.";
+
+        //Logger messages
+        public const string EntityWithIdWasNotFoundLoggerErrorMessage = "{0} with id {1} was not found!";
+    }
+}

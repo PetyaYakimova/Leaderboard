ca5ccb196787456d90e49ac3e824337b83966b51
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-07 09:05:46 +0300

    Add custom exceptions

---
+++ Leaderboard.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Leaderboard.Core.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        private const string DefaultMessage = "This entity could not be found!";
+
+        public EntityNotFoundException() : base(DefaultMessage) { }
+
+        public EntityNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ Leaderboard.Core/Exceptions/InvalidModelException.cs
@@ -0,0 +1,11 @@
+﻿namespace Leaderboard.Core.Exceptions
+{
+    public class InvalidModelException : Exception
+    {
+        private const string DefaultMessage = "The given model is invalid!";
+
+        public InvalidModelException() : base(DefaultMessage) { }
+
+        public InvalidModelException(string message) : base(message) { }
+    }
+}

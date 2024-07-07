725be012e9c419c39a0bfc5c3d2355d13e5bbb91
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-07 09:06:28 +0300

    Add base models for filtering records

---
+++ Leaderboard.Core/Models/BaseModels/AllEntitiesQueryBaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Leaderboard.Core.Constants.LimitConstants;
+
+namespace Leaderboard.Core.Models.BaseModels
+{
+	/// <summary>
+	/// A base class for all models used to get filtering criteria and pagination info. No added validation attributes.
+	/// </summary>
+	/// <typeparam name="Т">Type of model entities</typeparam>
+	public abstract class AllEntitiesQueryBaseModel<T>
+	{
+		[Display(Name = "Search by name")]
+		public string? SearchTerm { get; init; }
+
+		public int ItemsPerPage { get; init; } = DefaultNumberOfItemsPerPage;
+
+		public int CurrentPage { get; init; } = 1;
+
+		public int TotalItemCount { get; set; }
+
+		public IEnumerable<T> Entities { get; set; } = new List<T>();
+	}
+}
---
+++ Leaderboard.Core/Models/BaseModels/EntityQueryServiceBaseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Leaderboard.Core.Models.BaseModels
+{
+	/// <summary>
+	/// A base class for all models that get the total count of all entities and a collection of a certain amount of them to display them on pages. No added validation attributes.
+	/// </summary>
+	/// <typeparam name="T">Type of model entities</typeparam>
+	public class EntityQueryServiceBaseModel<T>
+	{
+		public int TotalCount { get; set; }
+
+		public IEnumerable<T> Entities { get; set; } = new List<T>();
+	}
+}

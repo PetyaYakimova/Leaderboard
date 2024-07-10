bb45a4994c676e32f5929c72ac9a3b7124401c4f
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-10 09:24:41 +0300

    Update login and register views and use constants for register model

--- Leaderboard/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Leaderboard/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,124 +14,126 @@ using static Leaderboard.Infrastructure.Constants.DataConstants;
 namespace Leaderboard.Areas.Identity.Pages.Account
 {
 	public class RegisterModel : PageModel
-    {
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IUserStore<ApplicationUser> _userStore;
-        private readonly IUserEmailStore<ApplicationUser> _emailStore;
-        private readonly ILogger<RegisterModel> _logger;
-        private readonly IOrganizationService _organizationService;
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly IUserStore<ApplicationUser> _userStore;
+		private readonly IUserEmailStore<ApplicationUser> _emailStore;
+		private readonly ILogger<RegisterModel> _logger;
+		private readonly IOrganizationService _organizationService;
 
-        public RegisterModel(
-            UserManager<ApplicationUser> userManager,
-            IUserStore<ApplicationUser> userStore,
-            SignInManager<ApplicationUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IOrganizationService organizationService)
-        {
-            _userManager = userManager;
-            _userStore = userStore;
-            _emailStore = GetEmailStore();
-            _signInManager = signInManager;
-            _logger = logger;
-            _organizationService = organizationService;
-        }
+		public RegisterModel(
+			UserManager<ApplicationUser> userManager,
+			IUserStore<ApplicationUser> userStore,
+			SignInManager<ApplicationUser> signInManager,
+			ILogger<RegisterModel> logger,
+			IOrganizationService organizationService)
+		{
+			_userManager = userManager;
+			_userStore = userStore;
+			_emailStore = GetEmailStore();
+			_signInManager = signInManager;
+			_logger = logger;
+			_organizationService = organizationService;
+		}
 
-        [BindProperty]
-        public InputModel Input { get; set; }
+		[BindProperty]
+		public InputModel Input { get; set; }
 
-        public string ReturnUrl { get; set; }
+		public string ReturnUrl { get; set; }
 
-        public class InputModel
-        {
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
+		public class InputModel
+		{
+			[Required]
+			[EmailAddress]
+			[Display(Name = "Email")]
+			public string Email { get; set; }
 
-            [Required]
-            [StringLength(OrganizationNameMaxLength,
-                MinimumLength = OrganizatioNameMinLength,
-                ErrorMessage = StringLengthBetweenValuesMessage)]
-            [Display(Name = "Organization Name")]
-            public string OrganizationName { get; set; }
+			[Required]
+			[StringLength(OrganizationNameMaxLength,
+				MinimumLength = OrganizatioNameMinLength,
+				ErrorMessage = StringLengthBetweenValuesMessage)]
+			[Display(Name = "Organization name")]
+			public string OrganizationName { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
+			[Required]
+			[StringLength(UserPasswordMaxLength,
+				MinimumLength = UserPasswordMinLength,
+				ErrorMessage = StringLengthBetweenValuesMessage)]
+			[DataType(DataType.Password)]
+			[Display(Name = "Password")]
+			public string Password { get; set; }
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
-        }
+			[DataType(DataType.Password)]
+			[Display(Name = "Confirm password")]
+			[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+			public string ConfirmPassword { get; set; }
+		}
 
 
-        public void OnGetAsync(string returnUrl = null)
-        {
-            ReturnUrl = returnUrl;
-        }
+		public void OnGetAsync(string returnUrl = null)
+		{
+			ReturnUrl = returnUrl;
+		}
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl ??= Url.Content("~/");
+		public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+		{
+			returnUrl ??= Url.Content("~/");
 
-            if (ModelState.IsValid == false)
-            {
-                return Page();
-            }
+			if (ModelState.IsValid == false)
+			{
+				return Page();
+			}
 
-            else if (ModelState.IsValid)
-            {
-                var user = CreateUser();
+			else if (ModelState.IsValid)
+			{
+				var user = CreateUser();
 
-                user.OrganizationId = await _organizationService.CreateOrganizationAsync(Input.OrganizationName);
-                user.CanAddUsers = true;
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+				user.OrganizationId = await _organizationService.CreateOrganizationAsync(Input.OrganizationName);
+				user.CanAddUsers = true;
+				await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+				await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
 
-                var result = await _userManager.CreateAsync(user, Input.Password);
+				var result = await _userManager.CreateAsync(user, Input.Password);
 
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
+				if (result.Succeeded)
+				{
+					_logger.LogInformation("User created a new account with password.");
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl);
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
+					await _signInManager.SignInAsync(user, isPersistent: false);
+					return LocalRedirect(returnUrl);
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
 
-            // If we got this far, something failed, redisplay form
-            return Page();
-        }
+			// If we got this far, something failed, redisplay form
+			return Page();
+		}
 
-        private ApplicationUser CreateUser()
-        {
-            try
-            {
-                return Activator.CreateInstance<ApplicationUser>();
-            }
-            catch
-            {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
-                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
-            }
-        }
+		private ApplicationUser CreateUser()
+		{
+			try
+			{
+				return Activator.CreateInstance<ApplicationUser>();
+			}
+			catch
+			{
+				throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
+					$"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+					$"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+			}
+		}
 
-        private IUserEmailStore<ApplicationUser> GetEmailStore()
-        {
-            if (!_userManager.SupportsUserEmail)
-            {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
-            }
-            return (IUserEmailStore<ApplicationUser>)_userStore;
-        }
-    }
+		private IUserEmailStore<ApplicationUser> GetEmailStore()
+		{
+			if (!_userManager.SupportsUserEmail)
+			{
+				throw new NotSupportedException("The default UI requires a user store with email support.");
+			}
+			return (IUserEmailStore<ApplicationUser>)_userStore;
+		}
+	}
 }

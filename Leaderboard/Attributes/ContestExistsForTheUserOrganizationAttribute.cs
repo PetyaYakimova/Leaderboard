f82b800d7c804e10ca425da2fb65f2986dd69ef1
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-10 09:25:53 +0300

    Add custom action filter attributes

--- Leaderboard/Attributes/ContestExistsForTheUserOrganizationAttribute.cs
+++ Leaderboard/Attributes/ContestExistsForTheUserOrganizationAttribute.cs
@@ -1,9 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Leaderboard.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
 
 namespace Leaderboard.Attributes
 {
 	public class ContestExistsForTheUserOrganizationAttribute : ActionFilterAttribute
 	{
-		//TODO: Check how it is done in the ChoreographyBuilder
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IContestService? contestService = context.HttpContext.RequestServices.GetService<IContestService>();
+			IOrganizationService? organizationService = context.HttpContext.RequestServices.GetService<IOrganizationService>();
+
+			if (contestService == null || organizationService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			object? value = context.HttpContext.GetRouteData().Values["id"];
+			if (value == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+			}
+
+			if (value != null)
+			{
+				Guid id = new Guid();
+				if (Guid.TryParse(value.ToString(), out id))
+				{
+					if (contestService != null && contestService.ContestExistsByIdAsync(id).Result == false)
+					{
+						context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+					}
+
+					Guid organizationId = organizationService.GetUserOrganizationIdAsync(context.HttpContext.User.Id()).Result;
+
+					if (contestService.ContestExistsForOrganizationsByIdAsync(id, organizationId).Result == false)
+					{
+						context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+					}
+				}
+				else
+				{
+					context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+				}
+			}
+		}
 	}
 }
---
+++ Leaderboard/Attributes/UserCanAddUsersAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Leaderboard.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace Leaderboard.Attributes
+{
+	public class UserCanAddUsersAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IOrganizationService? service = context.HttpContext.RequestServices.GetService<IOrganizationService>();
+
+			if (service == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			var userId = context.HttpContext.User.Id();
+
+			if (service.CanUserAddUsersAsync(userId).Result == false)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+			}
+		}
+	}
+}

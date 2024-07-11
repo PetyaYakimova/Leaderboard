7f093bb65fbc543e8c45759371e6edecb30074af
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-11 08:42:03 +0300

    Add ceate administrators methods in organization controller

--- Leaderboard/Controllers/OrganizationController.cs
+++ Leaderboard/Controllers/OrganizationController.cs
@@ -1,47 +1,72 @@
-﻿using Leaderboard.Core.Contracts;
+﻿using Leaderboard.Attributes;
+using Leaderboard.Core.Contracts;
 using Leaderboard.Core.Models.Organization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System.Security.Claims;
 
 namespace Leaderboard.Controllers
 {
-    public class OrganizationController : BaseController
-    {
-        private readonly ILogger<OrganizationController> logger;
-        private readonly IOrganizationService organizationService;
+	public class OrganizationController : BaseController
+	{
+		private readonly ILogger<OrganizationController> logger;
+		private readonly IOrganizationService organizationService;
 
-        public OrganizationController(ILogger<OrganizationController> logger, IOrganizationService organizationService)
-        {
-            this.logger = logger;
-            this.organizationService = organizationService;
-        }
+		public OrganizationController(ILogger<OrganizationController> logger, IOrganizationService organizationService)
+		{
+			this.logger = logger;
+			this.organizationService = organizationService;
+		}
 
 		[HttpGet]
 		public async Task<IActionResult> Index()
-        {
-            Guid organizationId = await organizationService.GetUserOrganizationIdAsync(User.Id());
-            var model = await organizationService.GetOrganizationInfoAsync(organizationId);
-            
-            return View(model);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Administrators([FromQuery] AllUsersQueryModel query)
-        {
+		{
 			Guid organizationId = await organizationService.GetUserOrganizationIdAsync(User.Id());
-            var model = await organizationService.GetAllUsersAsync(
-                organizationId,
-                query.SearchTerm,
-                query.CurrentPage,
-                query.ItemsPerPage);
+			var model = await organizationService.GetOrganizationInfoAsync(organizationId);
 
-            query.TotalItemCount = model.TotalCount;
-            query.Entities = model.Entities;
+			return View(model);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Administrators([FromQuery] AllUsersQueryModel query)
+		{
+			Guid organizationId = await organizationService.GetUserOrganizationIdAsync(User.Id());
+			var model = await organizationService.GetAllUsersAsync(
+				organizationId,
+				query.SearchTerm,
+				query.CurrentPage,
+				query.ItemsPerPage);
+
+			query.TotalItemCount = model.TotalCount;
+			query.Entities = model.Entities;
 
 			return View(query);
 		}
 
-        //TODO: continue with adding users - inject organization service to see if the add user button should be displayed
+		[HttpGet]
+		[UserCanAddUsers]
+		public IActionResult AddAdministrator()
+		{
+			UserFormViewModel model = new UserFormViewModel();
+
+			return View(model);
+		}
+
+		[HttpPost]
+		[UserCanAddUsers]
+		public async Task<IActionResult> AddAdministrator(UserFormViewModel model)
+		{
+			if (ModelState.IsValid == false)
+			{
+				return View(model);
+			}
+
+			Guid organizationId = await organizationService.GetUserOrganizationIdAsync(User.Id());
+
+			await organizationService.AddUserAsync(model, organizationId);
+
+			return RedirectToAction(nameof(Administrators));
+		}
 	}
 }

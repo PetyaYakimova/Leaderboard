18e505243dd3fd533e28c57c67e8613c62806ec3
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-12 13:28:51 +0300

    Add methods for existing contest

--- Leaderboard.Core/Contracts/IContestService.cs
+++ Leaderboard.Core/Contracts/IContestService.cs
@@ -5,5 +5,9 @@ namespace Leaderboard.Core.Contracts
 	public interface IContestService
 	{
 		Task<Guid> CreateContestAsync(ContestFormViewModel model, Guid organizationId);
+
+		Task<bool> ContestExistsByIdAsync(Guid contestId);
+
+		Task<bool> ContestExistsForOrganizationsByIdAsync(Guid contestId, Guid organizationId);
 	}
 }
--- Leaderboard.Core/Services/ContestService.cs
+++ Leaderboard.Core/Services/ContestService.cs
@@ -2,6 +2,7 @@
 using Leaderboard.Core.Models.Contest;
 using Leaderboard.Infrastructure.Data.Common;
 using Leaderboard.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Leaderboard.Core.Services
 {
@@ -12,6 +13,19 @@ namespace Leaderboard.Core.Services
 		public ContestService(IRepository repository)
 		{
 			this.repository = repository;
+		}
+
+		public async Task<bool> ContestExistsByIdAsync(Guid contestId)
+		{
+			return await repository.AllAsReadOnly<Contest>()
+				.AnyAsync(c => c.Id == contestId);
+		}
+
+		public async Task<bool> ContestExistsForOrganizationsByIdAsync(Guid contestId, Guid organizationId)
+		{
+			return await repository.AllAsReadOnly<Contest>()
+				.Where(c => c.Id == contestId)
+				.AnyAsync(c => c.OrganizationId == organizationId);
 		}
 
 		public async Task<Guid> CreateContestAsync(ContestFormViewModel model, Guid organizationId)

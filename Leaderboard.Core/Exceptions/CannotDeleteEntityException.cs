bb4544200dcbfa78ecefbe579e5932ce18171de7
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-17 09:04:51 +0300

    Add custom exception for cannot delete entity

---
+++ Leaderboard.Core/Exceptions/CannotDeleteEntityException.cs
@@ -0,0 +1,11 @@
+﻿namespace Leaderboard.Core.Exceptions
+{
+	public class CannotDeleteEntityException : Exception
+	{
+		private const string DefaultMessage = "This entity could not be deleted!";
+
+		public CannotDeleteEntityException() : base(DefaultMessage) { }
+
+		public CannotDeleteEntityException(string message) : base(message) { }
+	}
+}

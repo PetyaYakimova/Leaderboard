9d7e5d688d11ed4a07f9e27b59de393edfa14886
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-23 08:30:47 +0300

    Add contest details view

---
+++ Leaderboard.Core/Models/Contest/ContestDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// View model only for previewing contests in details. No added validation attributes.
+	/// </summary>
+	public class ContestDetailsViewModel : ContestTableViewModel
+	{
+		public string? Description { get; set; }
+
+		public IEnumerable<TeamResultTableViewModel> Teams { get; set; } = new List<TeamResultTableViewModel>();
+	}
+}

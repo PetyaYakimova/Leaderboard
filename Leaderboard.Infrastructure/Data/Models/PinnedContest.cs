3c73160448809cb1ef3addea9909a49b3c5950f9
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-26 08:53:04 +0300

    Add pinned contest db table

---
+++ Leaderboard.Infrastructure/Data/Models/PinnedContest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Leaderboard.Infrastructure.Data.Models
+{
+	[Comment("Pinned Contests For Users")]
+	public class PinnedContest
+	{
+		[Required]
+		[Comment("Contest Identifier")]
+		public Guid ContestId { get; set; }
+
+		[ForeignKey(nameof(ContestId))]
+		public Contest Contest { get; set; } = null!;
+
+		[Required]
+		[Comment("User Identifier")]
+		public string UserId { get; set; } = string.Empty;
+
+		[ForeignKey(nameof(UserId))]
+		public ApplicationUser User { get; set; } = null!;
+	}
+}

480b5388cbb1f807279af37dd1cc0329441ff54b
PetyaYakimova <petya_yakimova@abv.bg>
2024-07-30 08:48:16 +0300

    Add point table view model

---
+++ Leaderboard.Core/Models/Contest/PointTableViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// View model only for previewing points for a team in a table. No added validation attributes.
+	/// </summary>
+	public class PointTableViewModel
+	{
+		public int Points { get; set; }
+
+		public string? Description { get; set; }
+
+		public string AddedByUserWithEmail { get; set; } = string.Empty;
+	}
+}

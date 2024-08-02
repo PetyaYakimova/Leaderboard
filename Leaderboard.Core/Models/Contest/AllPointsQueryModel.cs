d46d6cdfa09a81ad326ae92286d8f4274cd8e3d0
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-02 08:54:45 +0300

    Add all points query model

---
+++ Leaderboard.Core/Models/Contest/AllPointsQueryModel.cs
@@ -0,0 +1,16 @@
+﻿using Leaderboard.Core.Models.BaseModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Leaderboard.Core.Models.Contest
+{
+	public class AllPointsQueryModel : AllEntitiesQueryBaseModel<PointTableViewModel>
+	{
+		[Display(Name = "Search by points")]
+		public int? SearchPoints { get; init; }
+
+		[Display(Name = "Search by user email")]
+		public string? SearchUserEmail { get; init; }
+
+		public string TeamName { get; set; } = string.Empty;
+	}
+}

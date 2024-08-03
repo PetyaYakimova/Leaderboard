68340de0f74940d97fb7aaaed848cd4612eaeeff
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-03 09:10:28 +0300

    Add pinned contest view model

---
+++ Leaderboard.Core/Models/Contest/PinnedContestsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// A view model only for displaying the pinned contests and adding option to pin unpinned contest. No added validation attributes.
+	/// </summary>
+	public class PinnedContestsViewModel
+	{
+		public IEnumerable<ContestTableViewModel> PinnedContests { get; set; } = new List<ContestTableViewModel>();
+
+		public IEnumerable<ContestForPreviewViewModel> UnpinnedActiveContests { get; set; } = new List<ContestForPreviewViewModel>();
+	}
+}

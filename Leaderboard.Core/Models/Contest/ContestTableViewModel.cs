f77e5713025daa4218fc719a9a04d53ab0b2b66d
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-03 09:10:14 +0300

    Update contest table view model

--- Leaderboard.Core/Models/Contest/ContestTableViewModel.cs
+++ Leaderboard.Core/Models/Contest/ContestTableViewModel.cs
@@ -3,12 +3,8 @@
     /// <summary>
 	/// View model only for previewing contests in a table or when deleting them. No added validation attributes.
 	/// </summary>
-    public class ContestTableViewModel
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-
+    public class ContestTableViewModel : ContestForPreviewViewModel
+	{
         public bool IsActive { get; set; }
 
         public int NumberOfTeams { get; set; }

aee6a3a8343f3d700aa005af69405a206359b055
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-03 09:09:55 +0300

    Add contest for preview view model

---
+++ Leaderboard.Core/Models/Contest/ContestForPreviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// View model only for previewing basic contest info. No added validation attributes.
+	/// </summary>
+	public class ContestForPreviewViewModel
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+	}
+}

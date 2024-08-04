328de8f9f2ede2026c73f17a7730d1e92d487e79
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-04 08:57:25 +0300

    Add points query service model

---
+++ Leaderboard.Core/Models/Contest/PointsQueryServiceModel.cs
@@ -0,0 +1,11 @@
+﻿using Leaderboard.Core.Models.BaseModels;
+
+namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// A model that has the total count of points records and a collection of a certain amount of them to display them on pages. No added validation attributes.
+	/// </summary>
+	public class PointsQueryServiceModel : EntityQueryServiceBaseModel<PointTableViewModel>
+	{
+	}
+}

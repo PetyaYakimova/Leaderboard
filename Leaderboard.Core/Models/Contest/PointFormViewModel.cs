889deed9511bc0dbe1ab9534a0db5acfc36c1d24
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-04 08:57:07 +0300

    Add point form view model

---
+++ Leaderboard.Core/Models/Contest/PointFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using static Leaderboard.Infrastructure.Constants.DataConstants;
+using static Leaderboard.Core.Constants.MessagesConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// A view model for the form when adding points for a team. Added validation attributes.
+	/// </summary>
+	public class PointFormViewModel
+	{
+		[Required(ErrorMessage = RequieredMessage)]
+		[Range(PointsMin,
+			PointsMax,
+			ErrorMessage = NumberMustBeInRangeErrorMessage)]
+		public int Points { get; set; }
+
+		[StringLength(PointsDescriptionMaxLength,
+			ErrorMessage = StringLengthNoMoreThanValueMessage)]
+		public string? Description { get; set; }
+
+		public string TeamName { get; set; } = null!;
+	}
+}

4c8ee39618864c5b1aeeec0c3997db9e31683da8
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-05 10:25:59 +0300

    Add team leaderboard tble view model

---
+++ Leaderboard.Core/Models/Contest/TeamLeaderboardTableViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// View model only for previewing team total result in a table. No added validation attributes.
+	/// </summary>
+	public class TeamLeaderboardTableViewModel
+	{
+		public string Name { get; set; } = string.Empty;
+
+		public int TotalPoints { get; set; }
+	}
+}

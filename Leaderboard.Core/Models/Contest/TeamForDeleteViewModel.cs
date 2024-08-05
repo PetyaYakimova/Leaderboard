affe925ee64a25deabf4e0cb010cd01b3d63e5d5
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-05 10:25:30 +0300

    Add team for delete view model

---
+++ Leaderboard.Core/Models/Contest/TeamForDeleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// View model only for previewing team before deletion. No added validation attributes.
+	/// </summary>
+	public class TeamForDeleteViewModel
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public Guid ContestId { get; set; }
+	}
+}

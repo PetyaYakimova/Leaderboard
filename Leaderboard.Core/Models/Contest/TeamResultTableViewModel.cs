793e867ac687794663bf3b65f60f7a7a03171c94
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-06 10:14:20 +0300

    Add team result table view model

---
+++ Leaderboard.Core/Models/Contest/TeamResultTableViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Leaderboard.Core.Models.Contest
+{
+	/// <summary>
+	/// View model only for previewing team total result in a table. No added validation attributes.
+	/// </summary>
+	public class TeamResultTableViewModel
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public bool IsActive { get; set; }
+
+		public int TotalPoints { get; set; }
+	}
+}

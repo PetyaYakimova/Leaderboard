8b39046aae22d49d36e6c9f56f067878a579f179
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-14 15:28:43 +0300

    Reorder methods following CRUD order

--- Leaderboard.Infrastructure/Data/Common/IRepository.cs
+++ Leaderboard.Infrastructure/Data/Common/IRepository.cs
@@ -2,13 +2,11 @@
 {
 	public interface IRepository
 	{
+		Task AddAsync<T>(T entity) where T : class;
+
 		IQueryable<T> All<T>() where T : class;
 
 		IQueryable<T> AllAsReadOnly<T>() where T : class;
-
-		Task AddAsync<T>(T entity) where T : class;
-
-		Task<int> SaveChangesAsync();
 
 		Task<T?> GetByIdAsync<T>(object id) where T : class;
 
@@ -17,5 +15,7 @@
 		void Delete<T>(T entity) where T : class;
 
 		void DeleteRange<T>(IEnumerable<T> entities) where T : class;
+
+		Task<int> SaveChangesAsync();
 	}
 }

ae08480a989e41403eba1d1a0674659ce6b17491
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-15 11:11:27 +0300

    Reorder methods by CRUD order

--- Leaderboard.Core/Contracts/IContestService.cs
+++ Leaderboard.Core/Contracts/IContestService.cs
@@ -1,67 +1,78 @@
 ﻿using Leaderboard.Core.Models.Contest;
-using Leaderboard.Core.Models.Organization;
 using static Leaderboard.Core.Constants.LimitConstants;
 
 namespace Leaderboard.Core.Contracts
 {
 	public interface IContestService
 	{
+		//Create
+		#region
 		Task<Guid> CreateContestAsync(ContestFormViewModel model, Guid organizationId);
 
+		Task PinContestForUser(Guid contestId, string userId);
+
+		Task CreateTeamAsync(TeamFormViewModel model, Guid contestId);
+
+		Task CreatePointAsync(PointFormViewModel model, Guid teamId, string userId);
+		#endregion
+
+		//Read
+		#region
 		Task<bool> ContestExistsByIdAsync(Guid contestId);
 
 		Task<bool> ContestExistsForOrganizationsByIdAsync(Guid contestId, Guid organizationId);
 
-		Task<ContestQueryServiceModel> GetAllContestsForOrganizationAsync(Guid organizationId, string? searchedTerm = null, int? searchedNumberOfTeams = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
-
-		Task ChangeContestStatusAsync(Guid id);
-
-		Task<ContestFormViewModel> GetContestByIdAsync(Guid id);
-
-		Task EditContestAsync(Guid id, ContestFormViewModel model);
+		Task<bool> ContestHasNoTeamsAsync(Guid id);
 
 		Task<bool> ContestIsActiveAsync(Guid id);
 
-		Task<bool> ContestHasNoTeamsAsync(Guid id);
+		Task<ContestQueryServiceModel> GetAllContestsForOrganizationAsync(Guid organizationId, string? searchedTerm = null, int? searchedNumberOfTeams = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
 
-		Task<ContestForPreviewViewModel> GetContestForPreviewAsync(Guid id);
-
-		Task DeleteContestAsync(Guid id);
+		Task<ContestFormViewModel> GetContestByIdAsync(Guid id);
 
 		Task<ContestDetailsViewModel> GetContestDetailsAsync(Guid id);
 
+		Task<ContestForPreviewViewModel> GetContestForPreviewAsync(Guid id);
+
+		Task<Guid> GetContestForTeamByIdAsync(Guid teamId);
+
 		Task<ContestLeaderboardViewModel> GetContestLeaderboardAsync(Guid id);
 
-		Task PinContestForUser(Guid contestId, string userId);
-
-		Task UnpinContestForUser(Guid contestId, string userId);
-
-		Task CreateTeamAsync(TeamFormViewModel model, Guid contestId);
+		Task<PinnedContestsViewModel> GetUserPinnedAndUnpinnedContests(string userId);
 
 		Task<bool> TeamExistsByIdAsync(Guid teamId);
 
 		Task<bool> TeamExistsForOrganizationsByIdAsync(Guid teamId, Guid organizationId);
 
-		Task<TeamFormViewModel> GetTeamByIdAsync(Guid id);
-
-		Task EditTeamAsync(Guid id, TeamFormViewModel model);
-
-		Task<Guid> GetContestForTeamByIdAsync(Guid teamId);
-
-		Task ChangeTeamStatusAsync(Guid id);
-
-		Task DeleteTeamAsync(Guid id);
-
-		Task<TeamForDeleteViewModel> GetTeamForDeleteByIdAsync(Guid id);
-
 		Task<bool> TeamIsActiveAsync(Guid id);
 
-		Task CreatePointAsync(PointFormViewModel model, Guid teamId, string userId);
+		Task<TeamFormViewModel> GetTeamByIdAsync(Guid id);
+
+		Task<TeamForDeleteViewModel> GetTeamForDeleteByIdAsync(Guid id);
 
 		Task<string> GetTeamNameByIdAsync(Guid id);
 
 		Task<PointsQueryServiceModel> GetAllTeamPointsAsync(Guid teamId, string? searchedTerm = null, int? searchedPoints = null, string? searchedUserEmail = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
+		#endregion
 
-		Task<PinnedContestsViewModel> GetUserPinnedAndUnpinnedContests(string userId);
+		//Update
+		#region
+		Task ChangeContestStatusAsync(Guid id);
+
+		Task EditContestAsync(Guid id, ContestFormViewModel model);
+
+		Task ChangeTeamStatusAsync(Guid id);
+
+		Task EditTeamAsync(Guid id, TeamFormViewModel model);
+		#endregion
+
+		//Delete
+		#region
+		Task DeleteContestAsync(Guid id);
+
+		Task UnpinContestForUser(Guid contestId, string userId);
+
+		Task DeleteTeamAsync(Guid id);
+		#endregion
 	}
 }

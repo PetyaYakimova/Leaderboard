f6bde3adcefc613eaba9ccb020427172a3da5451
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-16 09:01:32 +0300

    Reorder methods by CRUD order

--- Leaderboard.Core/Contracts/IOrganizationService.cs
+++ Leaderboard.Core/Contracts/IOrganizationService.cs
@@ -5,18 +5,18 @@ namespace Leaderboard.Core.Contracts
 {
 	public interface IOrganizationService
 	{
+		Task<Guid> CreateOrganizationAsync(string organizationName);
+
+		Task AddUserAsync(UserFormViewModel model, Guid organizationId);
+
 		Task<OrganizationPreviewModel> GetOrganizationInfoAsync(Guid organizationId);
 
 		Task<bool> OrganizationExistsByIdAsync(Guid organizationId);
 
 		Task<Guid> GetUserOrganizationIdAsync(string userId);
 
-        Task<Guid> CreateOrganizationAsync(string organizationName);
-
 		Task<UserQueryServiceModel> GetAllUsersAsync(Guid organizationId, string? searchedTerm = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
 
 		Task<bool> CanUserAddUsersAsync(string userId);
-
-		Task AddUserAsync(UserFormViewModel model, Guid organizationId);
 	}
 }

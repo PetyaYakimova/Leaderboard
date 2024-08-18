dff8a8fd356cf5cf56f98627e598c84d3f8c8d06
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-18 09:22:50 +0300

    Add summary for entity query service base model

--- Leaderboard.Core/Models/BaseModels/EntityQueryServiceBaseModel.cs
+++ Leaderboard.Core/Models/BaseModels/EntityQueryServiceBaseModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace Leaderboard.Core.Models.BaseModels
 {
 	/// <summary>
-	/// A base class for all models that get the total count of all entities and a collection of a certain amount of them to display them on pages. No added validation attributes.
+	/// A base class for all models that get the total count of all entities and a collection of a certain amount of them to display them on pages. 
+	/// No added validation attributes.
 	/// </summary>
 	/// <typeparam name="T">Type of model entities</typeparam>
 	public class EntityQueryServiceBaseModel<T>

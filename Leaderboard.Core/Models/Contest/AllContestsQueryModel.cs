b09809601df740f71b5f40b781af08ae75b22cf8
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-18 09:23:10 +0300

    Add summary for all constests query model

--- Leaderboard.Core/Models/Contest/AllContestsQueryModel.cs
+++ Leaderboard.Core/Models/Contest/AllContestsQueryModel.cs
@@ -3,7 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Leaderboard.Core.Models.Contest
 {
-    public class AllContestsQueryModel : AllEntitiesQueryBaseModel<ContestTableViewModel>
+	/// <summary>
+	/// A contest query model used to get filtering criteria and pagination info. 
+	/// No added validation attributes.
+	/// </summary>
+	public class AllContestsQueryModel : AllEntitiesQueryBaseModel<ContestTableViewModel>
     {
         [Display(Name = "Number of teams")]
         public int? SearchNumberOfTeams { get; init; }

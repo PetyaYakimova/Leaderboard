87ec5fd3f78515ff9059591a07ab7b334135fd4f
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-19 08:45:40 +0300

    Add summary for contest details view model

--- Leaderboard.Core/Models/Contest/ContestDetailsViewModel.cs
+++ Leaderboard.Core/Models/Contest/ContestDetailsViewModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace Leaderboard.Core.Models.Contest
 {
 	/// <summary>
-	/// View model only for previewing contests in details. No added validation attributes.
+	/// View model only for previewing contests in details. 
+	/// No added validation attributes.
 	/// </summary>
 	public class ContestDetailsViewModel : ContestTableViewModel
 	{

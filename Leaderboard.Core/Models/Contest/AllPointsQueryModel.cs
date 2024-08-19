0cfb42751cf63af1df2400383334b5cbeef91e94
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-19 08:45:26 +0300

    Add sumamry for all points query model

--- Leaderboard.Core/Models/Contest/AllPointsQueryModel.cs
+++ Leaderboard.Core/Models/Contest/AllPointsQueryModel.cs
@@ -3,6 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Leaderboard.Core.Models.Contest
 {
+	/// <summary>
+	/// A points query model used to get filtering criteria and pagination info. 
+	/// No added validation attributes.
+	/// </summary>
 	public class AllPointsQueryModel : AllEntitiesQueryBaseModel<PointTableViewModel>
 	{
 		[Display(Name = "Search by points")]

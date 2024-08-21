2f79cc9e4b8f77218ee8bd6e6f3b51a1d305b70b
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-21 08:32:11 +0300

    Add summary for contest query service model

--- Leaderboard.Core/Models/Contest/ContestQueryServiceModel.cs
+++ Leaderboard.Core/Models/Contest/ContestQueryServiceModel.cs
@@ -3,7 +3,8 @@
 namespace Leaderboard.Core.Models.Contest
 {
     /// <summary>
-	/// A model that has the total count of contests and a collection of a certain amount of them to display them on pages. No added validation attributes.
+	/// A model that has the total count of contests and a collection of a certain amount of them to display them on pages.
+    /// No added validation attributes.
 	/// </summary>
     public class ContestQueryServiceModel : EntityQueryServiceBaseModel<ContestTableViewModel>
     {

913e29b107d967d62ef6d70eb7d8e3259fa5be8e
Petya Yakimova <petya_yakimova@abv.bg>
2024-09-10 10:27:17 +0300

    Add database mock

---
+++ Leaderboard.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿using Leaderboard.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leaderboard.Tests.Mocks
+{
+	public static class DatabaseMock
+	{
+		public static LeaderboardDbContext Instance
+		{
+			get
+			{
+				var dbContextOptions = new DbContextOptionsBuilder<LeaderboardDbContext>()
+					.UseInMemoryDatabase("LeaderboardInMemoryDb" + DateTime.Now.Ticks.ToString())
+					.Options;
+
+				return new LeaderboardDbContext(dbContextOptions);
+			}
+		}
+	}
+}

0f80c0aed1c75442f7f7d12b5fca0ce5eb074804
Petya Yakimova <petya_yakimova@abv.bg>
2024-09-11 09:47:23 +0300

    Add test for create organization method

---
+++ Leaderboard.Tests/UnitTests/OrganizationServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Leaderboard.Core.Contracts;
+using Leaderboard.Core.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Leaderboard.Tests.UnitTests
+{
+	[TestFixture]
+	public class OrganizationServiceTests : UnitTestsBase
+	{
+		private IOrganizationService organizationService;
+		private ILogger<OrganizationService> logger;
+
+		[SetUp]
+		public void Setup()
+		{
+			var mockLogger = new Mock<ILogger<OrganizationService>>();
+			this.logger = mockLogger.Object;
+
+			this.organizationService = new OrganizationService(this.logger, repository);
+		}
+
+		[Test]
+		public async Task CreateOrganization_ShouldCreateNewOrganization()
+		{
+			var organizationCountBefore = this.data.Organizations.Count();
+
+			string organizatioName = "New organization from test";
+
+			await organizationService.CreateOrganizationAsync(organizatioName);
+
+			var organizationCountAfter = this.data.Organizations.Count();
+			Assert.That(organizationCountAfter, Is.EqualTo(organizationCountBefore + 1));
+		}
+	}
+}

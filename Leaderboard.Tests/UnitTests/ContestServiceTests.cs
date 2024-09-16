ffd66e64cb8b5e0ee44a44442bccc993e7bfa9ef
Petya Yakimova <petya_yakimova@abv.bg>
2024-09-16 09:20:07 +0300

    Add contest service tests class

---
+++ Leaderboard.Tests/UnitTests/ContestServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Leaderboard.Core.Contracts;
+using Leaderboard.Core.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Leaderboard.Tests.UnitTests
+{
+	[TestFixture]
+	public class ContestServiceTests : UnitTestsBase
+	{
+		private IOrganizationService organizationService;
+		private IContestService contestService;
+
+		private ILogger<OrganizationService> organizationLogger;
+		private ILogger<ContestService> logger;
+
+		[SetUp]
+		public void Setup()
+		{
+			var mockOrganizationLogger = new Mock<ILogger<OrganizationService>>();
+			this.organizationLogger = mockOrganizationLogger.Object;
+
+			var mockLogger = new Mock<ILogger<ContestService>>();
+			this.logger = mockLogger.Object;
+
+			this.organizationService = new OrganizationService(this.organizationLogger, repository);
+
+			this.contestService = new ContestService(this.logger, repository, organizationService);
+		}
+
+	}
+}
